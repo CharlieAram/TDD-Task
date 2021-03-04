89f9bac0c8298fca41e9a8014d748239120de3b0
Charlie Aram <charlieosa21@outlook.com>
2021-03-04 15:00:23 +0000

    Initial Commit

---
+++ TDD Task/TDD_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDD_TaskApp.swift
+//  TDD Task
+//
+//  Created by Charlie Aram on 04/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct TDD_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
